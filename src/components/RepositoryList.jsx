187d0ecdbff0ba4b2711b9c4983d762a3ae0da3a
Mattheus <mattheuseinloft@gmail.com>
2022-02-16 18:27:12 -0300

    feat(components/RepositoryList): fetch repositories from Github API

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,6 +1,8 @@
+import { useState, useEffect } from 'react';
 import { RepositoryItem } from "./RepositoryItem";
 
 import '../styles/repositories.scss';
+import { useState } from "react";
 
 const repository = {
   name: 'unform',
@@ -9,6 +11,16 @@ const repository = {
 }
 
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.github.com/orgs/rocketseat/repos')
+      .then(response => response.json())
+      .then(data => setRepositories(data))
+  }, []);
+
+  console.log(repositories);
+
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
