fa47b20857d2ef90b42392ce9d79600ab76ee959
Mattheus <mattheuseinloft@gmail.com>
2022-02-16 18:44:46 -0300

    feat(components/RepositoryList): list repositories

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -2,13 +2,6 @@ import { useState, useEffect } from 'react';
 import { RepositoryItem } from "./RepositoryItem";
 
 import '../styles/repositories.scss';
-import { useState } from "react";
-
-const repository = {
-  name: 'unform',
-  description: 'Forms in React',
-  link: 'https://github.com/unform/unform'
-}
 
 export function RepositoryList() {
   const [repositories, setRepositories] = useState([]);
@@ -19,17 +12,14 @@ export function RepositoryList() {
       .then(data => setRepositories(data))
   }, []);
 
-  console.log(repositories);
-
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
 
       <ul>
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
+        {repositories.map(repository => {
+          return <RepositoryItem key={repository.name} repository={repository} />;
+        })}
       </ul>
     </section>
   );
